4e39bb84f072d7732b6dc053f182179aaac265a3
Jack <jack.jh@protonmail.com>
2018-10-09 20:06:54 +0000

    added a 16*16 tile system

--- src/Screen.java
+++ src/Screen.java
@@ -1,12 +1,21 @@
+import java.beans.ParameterDescriptor;
+import java.util.Random;
+
 public class Screen {
 
     private int width, height;
     public int[] pixels;
+    public int[] tiles = new int[64*64];
+
+    private Random random = new Random();
 
     public Screen(int width, int height){
         this.width = width;
         this.height = height;
         pixels = new int[width * height]; // 50,400
+//        for(int i=0; i< 64*64; i++){
+//            tiles[i]=random.nextInt(0xffffff); // give each tile a random color when class is created
+//        }
     }
 
     // Clean the screen
@@ -17,10 +26,15 @@ public class Screen {
     }
 
     public void render(){
+        for(int i=0; i< 64*64; i++){
+            tiles[i]=random.nextInt(0xffffff); // give each tile a random color when class is created
+        }
         for(int y = 0; y<height; y++){
+            if(y<0 || y> height) break;
             for(int x = 0; x<width; x++){
-                //pixels[x+y*width] = 0xff00ff;
-                pixels[40+2*width] = 0xff000ff;
+                if(x<0 || x>width) break;
+                int tileIndex = (x/16) + (y/16) * 64;
+                pixels[x+y*width] = tiles[tileIndex];
             }
         }
     }

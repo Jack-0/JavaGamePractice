3eebb85a549bdf4b9871ce656dbf8c71baf9f8a3
Jack <jack.jh@protonmail.com>
2018-10-09 21:05:38 +0000

    improvements to tile system code

--- src/Screen.java
+++ src/Screen.java
@@ -5,7 +5,9 @@ public class Screen {
 
     private int width, height;
     public int[] pixels;
-    public int[] tiles = new int[64*64];
+    public final int MAP_SIZE = 8;
+    public final int MAP_SIZE_MASK = MAP_SIZE - 1;
+    public int[] tiles = new int[MAP_SIZE*MAP_SIZE];
 
     private Random random = new Random();
 
@@ -13,9 +15,10 @@ public class Screen {
         this.width = width;
         this.height = height;
         pixels = new int[width * height]; // 50,400
-//        for(int i=0; i< 64*64; i++){
-//            tiles[i]=random.nextInt(0xffffff); // give each tile a random color when class is created
-//        }
+        for(int i=0; i< MAP_SIZE*MAP_SIZE; i++){
+            tiles[i]=random.nextInt(0xffffff); // give each tile a random color when class is created
+            tiles[0]=0; // set first tiles to black
+        }
     }
 
     // Clean the screen
@@ -26,15 +29,15 @@ public class Screen {
     }
 
     public void render(){
-        for(int i=0; i< 64*64; i++){
-            tiles[i]=random.nextInt(0xffffff); // give each tile a random color when class is created
-        }
-        for(int y = 0; y<height; y++){
-            if(y<0 || y> height) break;
-            for(int x = 0; x<width; x++){
-                if(x<0 || x>width) break;
-                int tileIndex = (x/16) + (y/16) * 64;
-                pixels[x+y*width] = tiles[tileIndex];
+        for(int y=0; y<height; y++){
+            int yy = y;
+            //if(yy < 0 || yy > height) break;
+            for(int x=0; x<width; x++){
+                int xx = x;
+                //if(xx < 0 || xx > width) break;
+                int tileIndex = ((xx >> 4) & MAP_SIZE_MASK) + ((yy >> 4) & MAP_SIZE_MASK) * MAP_SIZE; // ensure index
+                                                                                                      // doesn't out_bnd
+                pixels[x + y * width] = tiles[tileIndex];
             }
         }
     }
